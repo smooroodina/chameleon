fd0f5cdb92f1f32dc89b5d41b85c91eec96f38da
202002575 <eogkrry2020@gmail.com>
2023-03-10 22:26:53 +0900

    saturation cast(0~255) & Hue Problem fixed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cv2
 import rainbow
 
 if __name__ == '__main__':
-    src = cv2.imread('image.png', cv2.IMREAD_COLOR)
+    src = cv2.imread('image.png', cv2.IMREAD_COLOR, )
     src_hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
     src_hsv = rainbow.change_saturation(src_hsv, 5)
     rainbow.dynamic_hue_video(src_hsv, 45, 1)
--- rainbow.py
+++ rainbow.py
@@ -5,21 +5,17 @@ import cv2
 import random
 
 def change_hue_image(img_hsv, hue_change):
-    def set_new_hue(row):
-        new_row = copy.deepcopy(row)
-        new_row[:, 0] = (row[:, 0] + hue_change) % 360
-        return new_row
-    result_hsv = np.array(list(map(set_new_hue, img_hsv)))
-    return result_hsv
+    img_hsv[:, :, 0] = (img_hsv[:, :, 0]+hue_change)%180
+    if hue_change % 10 == 0:
+        print(img_hsv[:, :, 0])
+    return img_hsv
 
 def random_hue_image(img_hsv):
     return change_hue_image(img_hsv, random.randrange(0, 360));
 
 def change_saturation(img_hsv, rate:float):
-    def low_saturation(row):
-        row[:,1] = row[:,1]*rate
-        return row
-    result_hsv = np.array(list(map(low_saturation, img_hsv)))
+    result_hsv = copy.deepcopy(img_hsv)
+    result_hsv[:, :, 1] = np.clip((result_hsv[:, :, 1].astype(np.float32)*rate), 0, 255)
     return result_hsv
 
 def dynamic_hue_video(img_hsv, fps:int=30, degree:int=1):
@@ -27,7 +23,7 @@ def dynamic_hue_video(img_hsv, fps:int=30, degree:int=1):
     h, w, c = img_hsv.shape
     size = (w, h)
     out = cv2.VideoWriter('./dynamicHue.mp4', cv2.VideoWriter_fourcc(*codec), fps, size)
-    for hue_change in range(degree, 360):
-        img_new = cv2.cvtColor(change_hue_image(img_hsv, hue_change), cv2.COLOR_HSV2BGR)
+    for i in range(180//degree):
+        img_new = cv2.cvtColor(change_hue_image(img_hsv, degree), cv2.COLOR_HSV2BGR)
         out.write(img_new)
     out.release()
