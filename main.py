0468dc9a447b4bb7402b41d65d355d63dadf9a0b
smooroodina <redcurrantcluster@gmail.com>
2023-03-11 15:52:24 +0900

    add gif creation method

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import numpy as np
 import cv2
 import rainbow
 
 if __name__ == '__main__':
     src = cv2.imread('./img/image.png', cv2.IMREAD_COLOR, )
     src_hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
-    src_hsv = rainbow.change_saturation(src_hsv, 1)
-    rainbow.dynamic_hue_video('./img//dynamicHue_low.mp4', src_hsv, 45, 1)
+    src_hsv = rainbow.change_saturation(src_hsv, 1.2)
+    rainbow.dynamic_hue_video('./img//dynamicHue_high.gif', src_hsv, 45, 1)
--- rainbow.py
+++ rainbow.py
@@ -1,8 +1,9 @@
-import copy
 
 import numpy as np
 import cv2
+import imageio
 import random
+
 
 def change_hue_image(img_hsv, hue_change):
     img_hsv[:, :, 0] = (img_hsv[:, :, 0]+hue_change)%180
@@ -14,16 +15,23 @@ def random_hue_image(img_hsv):
     return change_hue_image(img_hsv, random.randrange(0, 360));
 
 def change_saturation(img_hsv, rate:float):
-    result_hsv = copy.deepcopy(img_hsv)
+    result_hsv = img_hsv.copy()
     result_hsv[:, :, 1] = np.clip((result_hsv[:, :, 1].astype(np.float32)*rate), 0, 255)
     return result_hsv
 
 def dynamic_hue_video(filepath, img_hsv, fps:int=30, degree:int=1):
-    codec = 'avc1'
-    h, w, c = img_hsv.shape
-    size = (w, h)
-    out = cv2.VideoWriter(filepath, cv2.VideoWriter_fourcc(*codec), fps, size)
-    for i in range(180//degree):
-        img_new = cv2.cvtColor(change_hue_image(img_hsv, degree), cv2.COLOR_HSV2BGR)
-        out.write(img_new)
-    out.release()
+    file_extension = filepath.split('.')[-1]
+    if file_extension == 'mp4':
+        codec = 'avc1'
+        h, w, c = img_hsv.shape
+        size = (w, h)
+        out = cv2.VideoWriter(filepath, cv2.VideoWriter_fourcc(*codec), fps, size)
+        for i in range(180//degree):
+            img_new = cv2.cvtColor(change_hue_image(img_hsv, degree), cv2.COLOR_HSV2BGR)
+            out.write(img_new)
+        out.release()
+    elif file_extension == 'gif':
+        with imageio.get_writer(filepath, mode='I', fps=fps) as writer:
+            for i in range(180 // degree):
+                writer.append_data(cv2.cvtColor(change_hue_image(img_hsv, degree), cv2.COLOR_HSV2BGR))
+
